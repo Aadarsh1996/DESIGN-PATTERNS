b54ba6ba7d00cf3e1201725cefa42946fc3e20e7
Aadarsh Rajpurohit <you@example.com>
2024-02-09 11:12:48 +0530

    Singleton

---
+++ Singleton.py
@@ -0,0 +1,26 @@
+class Singleton(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super().__call__(*args,**kwargs)
+        return cls._instance[cls]
+class Logger(metaclass=Singleton):
+    def __init__(self):
+        print("Creating logger..")
+
+    def log(self,msg):
+        print(msg)
+
+class CustomLogger(Logger):
+    def __init__(self):
+        print("Creating custom logger")
+        super(CustomLogger, self).__init__()
+
+logger = CustomLogger()
+logger2 = CustomLogger()
+print(logger)
+print(logger2)
+logger.log(" First log")
+logger.log(" Second log")
+
